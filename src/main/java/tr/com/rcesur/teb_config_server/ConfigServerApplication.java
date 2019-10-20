ef07254e78488e85297fcd2736fc16835bc302c1
ramazancesur <ramazancesur@outlook.com>
2019-10-20 15:51:47 +0300

    first commit

---
+++ src/main/java/tr/com/rcesur/teb_config_server/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package tr.com.rcesur.teb_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by ramazancesur on 10/20/19.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
